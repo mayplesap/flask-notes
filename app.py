f5eac12d32af615df99005ada85ef073014141cf
ZeeTom <zthom13@hotmail.com>
2021-07-20 13:30:34 -0600

    added routes, need to complete /secret

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Flask note app."""
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
@@ -25,6 +25,45 @@ def root():
 
     redirect ("/register")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
+    """Displays a form to register a new user"""
+    
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+
+        user = User(username=form.username.data, password=form.password.data, email=form.email.data, first_name=form.first_name.data, last_name=form.last_name.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        session['username'] = user.username
+        return redirect('/secret')
+
+
+    return render_template('register.html', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    """Displays a form to login a new user"""
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+
+        if user:
+            session['username'] = user.username
+            return redirect('/secret')
+        else:
+            form.username.errors = ['Bad username/password']
+
+
+    return render_template('login.html', form=form)
 
